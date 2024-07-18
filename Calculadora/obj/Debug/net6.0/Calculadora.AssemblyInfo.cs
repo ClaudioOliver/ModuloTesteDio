c49f625ee36352bb6f5079b6da88ee3a08d773d9
ClaudioOliver <claudio.oliveira127@outlook.com>
2024-07-18 13:59:44 -0300

    Implementando Validação de String

--- Calculadora/obj/Debug/net6.0/Calculadora.AssemblyInfo.cs
+++ Calculadora/obj/Debug/net6.0/Calculadora.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b23163a380eacde25a9ab77ab25ebf5b45df876")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CalculadoraTestes/CalculadoraTests.cs
+++ CalculadoraTestes/CalculadoraTests.cs
@@ -26,4 +26,20 @@ public class CalculadoraTests
 
 
     }
+
+    [Fact]
+    public void DeveSomar10Com10ERetornar20()
+    {
+        //Arrange
+        int num1 = 10;
+        int num2 = 10;
+
+        //Act
+        int resuldado = _calc.Somar(num1, num2);
+
+        //Assert
+        Assert.Equal(20, resuldado);
+
+
+    }
 }
